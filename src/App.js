02db8e4f8dabb1958bf6311ec7e972cd31a34912
ecris87 <emilia.cristea@gmail.com>
2017-05-15 14:49:27 +0300

    Day Two :sparkles:

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-// import { Navbar, Jumbotron, Button } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 
 
 import Header from './Header';
@@ -8,14 +8,58 @@ import OtherComponent from './OtherComponent';
 // TODO More import examples
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      date: new Date(),
+      name: 'Emi',
+      counter: 0
+    };
+
+    this.changeName = this.changeName.bind(this);
+    this.increment = this.changeCounter.bind(this, 1);
+    this.decrement = this.changeCounter.bind(this, -1);
+  }
+
+  changeName(name) {
+    this.setState({name: name});
+  }
+
+  changeCounter(inc) {
+    console.log('before:', this.state.counter);
+    this.setState((prevState) => ({
+      counter: prevState.counter + inc
+    }));
+    this.setState((prevState) => ({
+      counter: prevState.counter + inc
+    }));
+    console.log('after:', this.state.counter);
+  }
+
+  componentDidMount() {
+    // this.timer = setInterval(() => {
+    //   this.setState({
+    //     date: new Date()
+    //   })
+    // }, 1000);
+  }
+
+  componentWillUnmount() {
+    // clearInterval(this.timer);
+  }
 
   render() {
-    const name = 'Dana Scully';
+    console.log('render App');
     return (
         <div className="app">
-           <h1>React playground</h1>
-           <Header name={name}/>
-           <OtherComponent name={name}/>
+           <h1>React playground {this.state.date.toLocaleTimeString()}</h1>
+           <Header name={this.state.name} changeName={this.changeName} isActive={true}/>
+           <OtherComponent name={this.state.name}/>
+
+           <br />
+           <Button bsStyle="primary" onClick={this.increment}> + </Button>
+           {' ' + this.state.counter + ' '}
+           <Button bsStyle="primary" onClick={this.decrement}> - </Button>
         </div>
     );
   }
--- src/Header.js
+++ src/Header.js
@@ -3,11 +3,22 @@ import React from 'react';
 import Title from './Title';
 
 class Header extends React.Component {
+    constructor() {
+        super();
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        this.props.changeName(event.target.value);
+    }
+
     render() {
         console.log('render Header');
         return (
             <div>
                 <Title name={this.props.name}/>
+                <input value={this.props.name} 
+                        onChange={this.handleChange} />
             </div>
         )
     }
