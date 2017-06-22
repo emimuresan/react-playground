af166beb794a49b81adbffb73e9ef2f80dc4e1d3
ecris87 <emilia.cristea@gmail.com>
2017-06-22 12:32:44 +0300

    Day5 :tada:

---
+++ src/day5/Image.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Image(props) {
+    return <img src={props.picture} alt="pic" />;
+}
+
+export default Image;
--- src/day5/Main.js
+++ src/day5/Main.js
@@ -4,21 +4,18 @@ import pic1 from '../img/221.jpeg';
 import pic2 from '../img/233.jpeg';
 import pic3 from '../img/377.jpeg';
 import pic4 from '../img/434.jpeg';
+import Image from './Image';
 
 
 class Main extends React.Component {
-  constructor() {
-    super();
-  }
-
   render() {
     return (
         <main>
-            <SlideShow>
-                <img src={pic1} alt="pic1" />
-                <img src={pic2} alt="pic2" />
-                <img src={pic3} alt="pic3" />
-                <img src={pic4} alt="pic4" />
+            <SlideShow name="Composition in React">
+                <Image picture={pic1} />
+                <Image picture={pic2} />
+                <Image picture={pic3} />
+                <Image picture={pic4} />
             </SlideShow>
         </main>
     );
